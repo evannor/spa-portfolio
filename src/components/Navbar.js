0ddcf857c1dafa2a5d8fd96c6f10112519e34953
evannor <erica.vannorman@gmail.com>
2021-02-09 17:16:47 -0800

    Add Navbar, clean up dropdown style

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav id="navbar">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          Projects <i className="far fa-caret-square-down"></i>
+          <ul>
+            <li>
+              <a href="#">Current</a>
+            </li>
+            <li>
+              <a href="#">Completed</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Resume</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
