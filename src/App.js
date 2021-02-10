922e99ac6bb8d83016bfcfa24ce21b4142ee1fd1
evannor <erica.vannorman@gmail.com>
2021-02-09 21:06:13 -0800

    Create footer template and basic style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import "./Typewriter";
 
 function App() {
+  const copyDate = new Date().getFullYear();
   return (
     <div>
       <header id="header-home">
@@ -183,6 +185,67 @@ function App() {
           tincidunt id aliquet risus.
         </p>
       </section>
+      <footer id="main-footer" className="py-2">
+        <div className="container footer-container">
+          <div>
+            <i className="fas fa-mountain fa-5x"></i>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit repudiandae debitis vitae culpa sequi earum harum
+              rerum, minima repellendus voluptatum aspernatur ea vel. Nostrum,
+              fuga.
+            </p>
+          </div>
+          <div>
+            <h3>Email Newsletter</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit repudiandae debitis vitae
+            </p>
+            <form>
+              <input type="email" placeholder="Enter Email" />
+              <input
+                type="submit"
+                value="Subscribe"
+                className="btn btn-primary"
+              />
+            </form>
+          </div>
+          <div>
+            <h3>Site Links</h3>
+            <ul className="list">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Projects</a>
+              </li>
+              <li>
+                <a href="#">Resume</a>
+              </li>
+              <li>
+                <a href="#">Contact Me</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Keep Up With My Work</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure cum
+              ipsam necessitatibus.
+            </p>
+            <a href="#" className="btn btn-secondary">
+              Follow Me Now!
+            </a>
+          </div>
+          <div>
+            <p>Copyright &copy; {copyDate}</p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
