cd906217f2609bc5b7dea66735f3123a1a4c4793
evannor <erica.vannorman@gmail.com>
2021-02-09 18:53:55 -0800

    Add typewriter functionality to header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import "./Typewriter";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <header>
-        <p>Happy New Portfolio!!</p>
+      <header id="header-home">
+        <Navbar />
+        <div className="header-content">
+          <h1>
+            I Am Erica The
+            <span
+              className="txt-type"
+              datawait="3000"
+              datawords='[" Full Stack Developer", " Streamer", " Animal Lover", " Outdoor Enthusiast"]'
+            ></span>
+          </h1>
+          <p className="lead">
+            I specialize in creating understandable, dyanmic, single and
+            multi-page applications via HTML, CSS, and Javascript
+          </p>
+        </div>
       </header>
+      <p>Happy New Portfolio!!</p>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,33 +3,35 @@ import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav id="navbar">
-      <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          Projects <i className="far fa-caret-square-down"></i>
-          <ul>
-            <li>
-              <a href="#">Current</a>
-            </li>
-            <li>
-              <a href="#">Completed</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#">Resume</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav id="navbar">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            Projects <i className="far fa-caret-square-down"></i>
+            <ul>
+              <li>
+                <a href="#">Current</a>
+              </li>
+              <li>
+                <a href="#">Completed</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Resume</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
