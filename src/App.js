03b9aa1f70406b4754f17ec674fc6f25c7839c1d
evannor <erica.vannorman@gmail.com>
2021-02-12 15:22:04 -0800

    Create Footer component and style for component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 import calc from "./images/calculator.jpg";
 import pomo from "./images/pomodoro.jpg";
@@ -12,7 +13,6 @@ import simon from "./images/simon-says.PNG";
 import sassPort from "./images/sass-port.JPG";
 
 function App() {
-  const copyDate = new Date().getFullYear();
   return (
     <div>
       <Header />
@@ -186,67 +186,7 @@ function App() {
           tincidunt id aliquet risus.
         </p>
       </section>
-      <footer id="main-footer" className="py-2">
-        <div className="footer-container">
-          <div>
-            <i className="fas fa-mountain fa-5x"></i>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Reprehenderit repudiandae debitis vitae culpa sequi earum harum
-              rerum, minima repellendus voluptatum aspernatur ea vel. Nostrum,
-              fuga.
-            </p>
-          </div>
-          <div>
-            <h3>Email Newsletter</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Reprehenderit repudiandae debitis vitae
-            </p>
-            <form>
-              <input type="email" placeholder="Enter Email" />
-              <input
-                type="submit"
-                value="Subscribe"
-                className="btn btn-primary"
-              />
-            </form>
-          </div>
-          <div>
-            <h3>Site Links</h3>
-            <ul className="list">
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Projects</a>
-              </li>
-              <li>
-                <a href="#">Resume</a>
-              </li>
-              <li>
-                <a href="#">Contact Me</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h2>Keep Up With My Work</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure cum
-              ipsam necessitatibus.
-            </p>
-            <a href="#" className="btn btn-secondary">
-              Follow Me Now!
-            </a>
-          </div>
-          <div>
-            <p>Copyright &copy; {copyDate}</p>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
