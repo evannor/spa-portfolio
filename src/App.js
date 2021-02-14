bb4d185e87855ef49d84d2837ba88bba504106e3
evannor <erica.vannorman@gmail.com>
2021-02-14 13:10:59 -0800

    Clean up footer styles

--- src/App.js
+++ src/App.js
@@ -151,41 +151,6 @@ function App() {
           </div>
         </div>
       </section>
-      <section>
-        <h1>Contact Me</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget dolor
-          morbi non arcu risus quis varius quam quisque. Pharetra massa massa
-          ultricies mi quis hendrerit dolor magna eget. Urna cursus eget nunc
-          scelerisque viverra mauris in aliquam. Et pharetra pharetra massa
-          massa ultricies mi quis hendrerit dolor. Nisi vitae suscipit tellus
-          mauris a diam. Donec enim diam vulputate ut pharetra sit. Aliquam
-          faucibus purus in massa tempor. Accumsan tortor posuere ac ut. At
-          lectus urna duis convallis convallis tellus id. At auctor urna nunc id
-          cursus metus aliquam eleifend. Sagittis purus sit amet volutpat
-          consequat. Enim sit amet venenatis urna cursus eget nunc. Tellus
-          integer feugiat scelerisque varius morbi. Sit amet volutpat consequat
-          mauris nunc congue nisi vitae suscipit. In arcu cursus euismod quis
-          viverra nibh cras. Commodo viverra maecenas accumsan lacus. Porta nibh
-          venenatis cras sed felis. Venenatis a condimentum vitae sapien
-          pellentesque habitant morbi tristique. Est ante in nibh mauris cursus
-          mattis molestie a iaculis. Eget velit aliquet sagittis id. Mauris in
-          aliquam sem fringilla. Fusce id velit ut tortor pretium viverra
-          suspendisse. Orci phasellus egestas tellus rutrum tellus pellentesque
-          eu tincidunt. Aliquet nibh praesent tristique magna sit. Urna nunc id
-          cursus metus aliquam eleifend. Mi proin sed libero enim sed faucibus.
-          Netus et malesuada fames ac turpis egestas maecenas pharetra. Gravida
-          quis blandit turpis cursus. Accumsan sit amet nulla facilisi morbi
-          tempus iaculis. Dui faucibus in ornare quam. Dis parturient montes
-          nascetur ridiculus mus. Vel pretium lectus quam id leo in vitae
-          turpis. Faucibus scelerisque eleifend donec pretium vulputate sapien
-          nec sagittis aliquam. Feugiat in ante metus dictum at tempor commodo.
-          Ut morbi tincidunt augue interdum velit euismod in pellentesque massa.
-          Velit ut tortor pretium viverra suspendisse. Lacus sed turpis
-          tincidunt id aliquet risus.
-        </p>
-      </section>
       <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,23 +6,18 @@ function Footer() {
   return (
     <footer id="main-footer" className="py-2">
       <div className="footer-container">
-        <div>
-          <i className="fas fa-mountain fa-5x"></i>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Reprehenderit repudiandae debitis vitae culpa sequi earum harum
-            rerum, minima repellendus voluptatum aspernatur ea vel. Nostrum,
-            fuga.
-          </p>
+        <div className="personal p-2">
+          <i className="fas fa-mountain fa-3x"></i>
+          <p>Erica VanNorman</p>
+          <p>Seattle, WA</p>
+          <p>erica.vannorman@gmail.com</p>
         </div>
-        <div>
-          <h3>Email Newsletter</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Reprehenderit repudiandae debitis vitae
-          </p>
+        <div className="footer-contact">
+          <h3>Contact Me</h3>
           <form>
+            <input type="text" placeholder="Full Name" />
             <input type="email" placeholder="Enter Email" />
+            <textarea rows="4" cols="50" placeholder="Write message here..." />
             <input
               type="submit"
               value="Subscribe"
@@ -50,12 +45,19 @@ function Footer() {
             </li>
           </ul>
         </div>
-        <div>
+        <div className="social">
           <h2>Keep Up With My Work</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure cum
-            ipsam necessitatibus.
-          </p>
+          <div className="social-icons">
+            <a href="#">
+              <i className="fab fa-github fa-2x"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-linkedin-in fa-2x"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-twitter fa-2x"></i>
+            </a>
+          </div>
           <a href="#" className="btn btn-secondary">
             Follow Me Now!
           </a>
